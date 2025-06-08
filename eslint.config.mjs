168b62a19c0e61c9739a917aaa6206bfde6c562c
Hugo <hugo.rodrigues.lopes@gmail.com>
2025-06-08 12:51:08 +0100

    0.1 setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    rules: {
+      "semi": ["error", "never"],
+      "indent": ["error", 2],
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
